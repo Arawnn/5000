5021528d89b938a64ca2cf4aea99e7bd5da49183
François <francois@tool4staffing.com>
2019-07-20 17:41:32 +0200

    First route and database connection

--- index.php
+++ index.php
@@ -2,5 +2,28 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-echo "Salut Fanfouer!";
+use Illuminate\Database\Capsule\Manager as DB;
+use \Slim\Slim as Slim;
 
+// Configures database
+$db = new DB();
+$db->addConnection(parse_ini_file('src/conf/config.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
+
+session_start();
+
+// Creates Slim instance 
+$app = new Slim();
+$app->config(['routes.case_sensitive' => false]);
+
+
+// Home page
+$app->get('/', function() {
+	echo '<h1> The 5000 </h1>';
+	echo '<p>'.password_hash('123456',  PASSWORD_BCRYPT).'</p>';
+})->name('home');
+
+
+// Runs Slim
+$app->run();
