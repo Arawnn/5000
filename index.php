513664481f56b43d3fdb7edaf740d78008a86975
François <francois@tool4staffing.com>
2019-09-07 00:27:23 +0200

    Game server and client connection basis settled

--- index.php
+++ index.php
@@ -5,6 +5,12 @@ require __DIR__ . '/vendor/autoload.php';
 use Illuminate\Database\Capsule\Manager as DB;
 use \Slim\Slim as Slim;
 
+// use Ratchet\Server\IoServer;
+// use Ratchet\Http\HttpServer;
+// use Ratchet\WebSocket\WsServer;
+
+use The5000\model\Account as Account;
+
 use The5000\controller\AccountController as AccountController;
 
 // Configures database
@@ -22,7 +28,14 @@ $app->config(['routes.case_sensitive' => false]);
 
 // Home page
 $app->get('/', function() {
-	echo '<h1> The 5000 </h1>';
+    ?>
+    <h1> The 5000 </h1>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/game.css">
+    <input id="query_input" type="text" name="query_input" placeholder="Commandes..." disabled>
+    <p id="responses"></p>
+    <script src='script/game.js'></script>
+    <?php
 })->name('home');
 
 
---
+++ script/game.js
@@ -0,0 +1,35 @@
+
+let sock = new WebSocket( 'ws://192.168.1.17:15373/bin/game-server.php' );
+let div_responses = document.getElementById('responses');
+let input = document.getElementById('query_input');
+
+sock.onopen = function(e) {
+    div_responses.innerHTML += '<span class="success">Connexion établie !</span><br>';
+    input.disabled = false;
+};
+
+sock.onerror = function(e) {
+    console.log(e);
+    div_responses.innerHTML += '<span class="error">Une erreur est survenue...</span><br>';
+    input.disabled = true;
+};
+
+sock.onmessage = function(e) {
+    console.log(e);
+    div_responses.innerHTML += e.data + '<br>';
+};
+
+sock.onclose = function(e) {
+    console.log(e);
+    div_responses.innerHTML += '<span class="info">La connexion a été interrompue...</span><br>';
+    input.disabled = true;
+};
+
+document.getElementById('query_input').addEventListener('keydown', function(e) {
+    if (e.key == 'Enter') {
+        let query = input.value;
+        sock.send(query);
+        input.value = '';
+        console.log('Envoi de : ' + query);
+    }
+});
